460f4ebac362c54ea7a52534e50d07f6b3241fc6
Christian Vanderwall <christian@cvdub.net>
2023-04-21 12:21:08 -0700

    Add pyenv-mode

--- modules/init-python.el
+++ modules/init-python.el
@@ -3,6 +3,10 @@
   (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
   :hook (python-ts-mode . eglot-ensure))
 
+(use-package pyvenv
+  :init
+  (pyvenv-tracking-mode 1))
+
 (use-package poetry)
 
 (provide 'init-python)

9797304268838d7149d20d7e4c513fb09cced80c
Christian Vanderwall <christian@cvdub.net>
2023-04-21 18:10:59 -0700

    Add more Spacebase functionality

--- modules/init-keybindings.el
+++ modules/init-keybindings.el
@@ -3,4 +3,14 @@
 
 (global-set-key (kbd "C-c o") cjv/open-map)
 
+(defvar cjv/code-map (make-sparse-keymap)
+  "Keymap for code related commands.")
+
+(global-set-key (kbd "C-c c") cjv/code-map)
+
+(defvar cjv/my-map (make-sparse-keymap)
+  "Keymap for my personal commands.")
+
+(global-set-key (kbd "C-c m") cjv/my-map)
+
 (provide 'init-keybindings)
--- modules/init-spacebase.el
+++ modules/init-spacebase.el
@@ -27,4 +27,37 @@
                                   output-buffer
                                   command))))
 
+(defun spacebase/rebuild-test-db ()
+  "Regenerates Spacebase test databases."
+  (interactive)
+  (let* ((test-dbs '("test_spacebase_db"
+                     "test_spacebase_db_gw0"
+                     "test_spacebase_db_gw1"
+                     "test_spacebase_db_gw2"
+                     "test_spacebase_db_gw3"
+                     "test_spacebase_db_gw4"
+                     "test_spacebase_db_gw5"
+                     "test_spacebase_db_gw6"
+                     "test_spacebase_db_gw7"
+                     "test_spacebase_db_gw8"
+                     "test_spacebase_db_gw9"))
+         (sub-commands '("dropdb -U cjv --if-exists %s"
+                         "createdb -U cjv %s"
+                         "psql --quiet -U cjv -d %s -c \"create extension pg_trgm\""))
+         (command (cl-loop for command-string in sub-commands
+                           nconc (mapcar (lambda (db) (format command-string db)) test-dbs) into command
+                           finally (return (string-join command " && ")))))
+    (message "Rebuilding Spacebase test DB...")
+    (cjv/with-message
+     "Done!"
+     (start-process-shell-command "Rebuild Spacebase test DB" "*spacebase-rebuild-test-db*" command))))
+
+(defvar cjv/spacebase-map (make-sparse-keymap)
+  "Keymap for Spacebase commands.")
+
+(bind-key (kbd "s") cjv/spacebase-map cjv/my-map)
+
+(bind-key (kbd "d") #'spacebase/server-deploy cjv/spacebase-map)
+(bind-key (kbd "t") #'spacebase/rebuild-test-db cjv/spacebase-map)
+
 (provide 'init-spacebase)

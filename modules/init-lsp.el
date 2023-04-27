855d4db96d0c977cebdb2733902068378b75485a
Christian Vanderwall <christian@cvdub.net>
2023-04-27 11:53:49 -0700

    Exclude varibles from eglot Imenu

--- modules/init-lsp.el
+++ modules/init-lsp.el
@@ -2,6 +2,17 @@
   :straight (:type built-in)
   :bind (:map cjv/code-map
               ("f" . #'eglot-format-buffer))
+  :config
+  (defvar cjv/eglot-imenu-ignored-symbols '("Variable")
+    "Symbols excluded from eglot's Imenu.")
+
+  (defun cjv/eglot-imenu-filter (symbols)
+    "Filters Imenu result, excluding symbols in cjv/eglot-ignored-symbols."
+    (cl-remove-if (lambda (symbol-list)
+                    (member (car symbol-list) cjv/eglot-imenu-ignored-symbols))
+                  symbols))
+
+  (advice-add #'eglot-imenu :filter-return #'cjv/eglot-imenu-filter)
   :custom
   (eglot-events-buffer-size 0)
   (eglot-autoshutdown t))

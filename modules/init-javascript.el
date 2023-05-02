2b73b1c3d813f367d22c15484b926502491199e0
Christian Vanderwall <christian@cvdub.net>
2023-05-02 11:47:00 -0700

    Set JS indent offset to 4

--- modules/init-javascript.el
+++ modules/init-javascript.el
@@ -2,6 +2,8 @@
   :straight (:type built-in)
   :defer t
   :mode ("\\.ts\\'")
-  :hook (typescript-ts-mode . eglot-ensure))
+  :hook (typescript-ts-mode . eglot-ensure)
+  :custom
+  (typescript-ts-mode-indent-offset 4))
 
 (provide 'init-javascript)

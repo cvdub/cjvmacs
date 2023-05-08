abf2e59f1989aa940230d18f05b6a07d24809d8c
Christian Vanderwall <christian@cvdub.net>
2023-05-08 15:52:12 -0700

    Add more tramp options

--- modules/init-tramp.el
+++ modules/init-tramp.el
@@ -4,6 +4,7 @@
   (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
   :custom
   (tramp-default-method "ssh")
-  (tramp-backup-directory-alist nil))
+  (tramp-backup-directory-alist nil)
+  (tramp-use-ssh-controlmaster-options nil))
 
 (provide 'init-tramp)

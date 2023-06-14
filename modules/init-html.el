3afce700455611ebf452580ae533a3d7920c9160
Christian Vanderwall <christian@cvdub.net>
2023-06-14 16:28:54 -0700

    Disable pairing {/} in web mode

--- modules/init-html.el
+++ modules/init-html.el
@@ -7,6 +7,14 @@
          "\\.as[cp]x\\'"
          "\\.erb\\'"
          "\\.mustache\\'"
-         "\\.djhtml\\'"))
+         "\\.djhtml\\'")
+  :init
+  ;; Disable pairing {/}
+  (add-hook
+   'web-mode-hook
+   '(lambda ()
+      (setq-local electric-pair-inhibit-predicate
+                  (lambda (c)
+                    (if (char-equal c ?{) t (electric-pair-default-inhibit c)))))))
 
 (provide 'init-html)

f462720339992c9bc16ba5b903ca780b307843d8
Christian Vanderwall <christian@cvdub.net>
2023-06-14 16:21:35 -0700

    Add function for syncing emails

--- modules/init-email.el
+++ modules/init-email.el
@@ -1,16 +1,38 @@
 (use-package notmuch
   :defer t
   :init
+  (defvar cjv/email-sync-command "mbsync -Va && notmuch new")
+
   (defun cjv/notmuch-inbox ()
     "Opens the notmuch inbox."
     (interactive)
     (notmuch-search "tag:inbox" t))
+
+  (defun cjv/update-email ()
+    "Sync emails with server."
+    (interactive)
+    (let ((compilation-buffer-name-function (lambda (_) "*email update*")))
+      (with-current-buffer (compile cjv/email-sync-command)
+        (add-hook
+         'compilation-finish-functions
+         (lambda (buf status)
+           (if (equal status "finished\n")
+               (progn
+                 (delete-windows-on buf)
+                 (bury-buffer buf)
+                 (notmuch-refresh-all-buffers)
+                 (message "Email sync successful"))
+             (user-error "Failed to sync emails")))
+         nil
+         'local))))
+
   :hook ((notmuch-show-mode . mixed-pitch-mode)
          (notmuch-message-mode . mixed-pitch-mode)
          (notmuch-message-mode . visual-line-mode)
          (notmuch-message-mode . turn-off-auto-fill))
   :bind (:map cjv/open-map
-              ("m" . #'cjv/notmuch-inbox))
+              ("m" . #'cjv/notmuch-inbox)
+              ("M" . #'cjv/update-email))
   :config
   (remove-hook 'notmuch-show-hook #'notmuch-show-turn-on-visual-line-mode)
   :custom

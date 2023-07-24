9b91ae6436cdf34c2fe6b30757e17dbaef3922a2
Christian Vanderwall <christian@cvdub.net>
2023-07-24 11:30:13 -0700

    Add magit command to project.el dispatch

--- modules/init-project.el
+++ modules/init-project.el
@@ -10,8 +10,12 @@
                          (file-in-directory-p dir ignored-directory))
                        cjv/project-ignored-directories)
       (project-try-vc dir)))
-
   :custom
+  (project-switch-commands '((project-find-file "Find file")
+                             (project-find-regexp "Find regexp")
+                             (project-find-dir "Find directory")
+                             (magit-project-status "Magit" ?m)
+                             (project-eshell "Eshell")))
   (project-find-functions (list #'cjv/project-try-vc-with-ignored-directories)))
 
 (use-package projectile

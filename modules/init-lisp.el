23c70f42c38bf2c731ade883fbe0d8d712cdd290
Christian Vanderwall <christian@cvdub.net>
2023-11-08 10:10:00 -0800

    Configure sly

--- modules/init-lisp.el
+++ modules/init-lisp.el
@@ -5,6 +5,21 @@
          (lisp-interaction-mode . enable-paredit-mode)
          (scheme-mode . enable-paredit-mode)))
 
-(use-package sly)
+(use-package inf-lisp
+  :elpaca nil
+  :custom (inferior-lisp-program "sbcl"))
+
+
+(use-package sly
+  :bind (:map sly-mode-map
+              ("C-c o r" . cjv/lisp-open-repl))
+  :config
+  (defun cjv/lisp-open-repl ()
+    "Opens sly REPL in a bottom side window."
+    (interactive)
+    (cjv/with-bottom-window
+     (if (sly-connected-p)
+         (sly-mrepl)
+       (sly)))))
 
 (provide 'init-lisp)

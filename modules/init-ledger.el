956f43e56ae1d2c5c04555a7acd28a667d6752af
Christian Vanderwall <christian@cvdub.net>
2024-01-08 14:46:06 -0800

    Update ledger config for hledger

--- modules/init-ledger.el
+++ modules/init-ledger.el
@@ -1,4 +1,5 @@
 (use-package ledger-mode
+  :mode ("\\.csv.rules\\'" . ledger-mode)
   :custom
   (ledger-binary-path "hl")
   (ledger-report-links-in-register nil)
@@ -7,16 +8,17 @@
   (ledger-report-use-native-highlighting nil)
   (ledger-default-date-format "%Y-%m-%d")
   (ledger-post-amount-alignment-column 56)
-  (ledger-reports (("bal" "%(binary) [[ledger-mode-flags]] -f $(ledger-file) bal -V --infer-equity")
-                   ("reg" "%(binary) -f %(ledger-file) reg")
-                   ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
-                   ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
+  (ledger-reports '(("bal" "%(binary) [[ledger-mode-flags]] -f $(ledger-file) bal -V --infer-equity")
+                    ("bs" "%(binary) -f %(ledger-file) bs")
+                    ("reg" "%(binary) -f %(ledger-file) reg")
+                    ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+                    ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
 
 (use-package flymake-hledger
-  :after (ledger-mode flymake)
+  :after ledger-mode
   :hook (ledger-mode . flymake-hledger-enable)
   :custom
-  (flymake-hledger-command ("hl"))
+  (flymake-hledger-check-ledger-binary-path nil)
   (flymake-hledger-checks '("accounts" "commodities" "ordereddates")))
 
 (provide 'init-ledger)

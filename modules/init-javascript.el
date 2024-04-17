7435e8ff0b57b0e2f8198e81769be8c8f5cbcca5
Christian Vanderwall <christian@cvdub.net>
2024-04-17 11:56:45 -0700

    Change JS indent level to two spaces

--- modules/init-javascript.el
+++ modules/init-javascript.el
@@ -4,6 +4,12 @@
   :mode ("\\.ts\\'")
   :hook (typescript-ts-mode . eglot-ensure)
   :custom
-  (typescript-ts-mode-indent-offset 4))
+  (typescript-ts-mode-indent-offset 2))
+
+(use-package js-mode
+  :defer t
+  :ensure nil
+  :custom
+  (js-indent-level 2))
 
 (provide 'init-javascript)

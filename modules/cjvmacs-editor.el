34c47bfd1a434f876d3d39cda32c74162fda9d11
Christian Vanderwall <christian@cvdub.net>
2025-04-10 13:46:00 -0700

    Add json tree-sitter grammar

--- modules/cjvmacs-editor.el
+++ modules/cjvmacs-editor.el
@@ -146,7 +146,8 @@
                                         (rust "https://github.com/tree-sitter/tree-sitter-rust")
                                         (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
                                         (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-                                        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+                                        (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+                                        (json "https://github.com/tree-sitter/tree-sitter-json")))
   :custom
   (treesit-extra-load-path (list (expand-file-name "tree-sitter"
                                                    user-emacs-local-directory))))

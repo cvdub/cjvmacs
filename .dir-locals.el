279a144f6f9a6b32966777c063c468ef074a2358
Christian Vanderwall <christian@cvdub.net>
2025-10-02 13:48:07 -0700

    Set project scratch major mode to lisp-interaction-mode

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((project-scratch-major-mode . lisp-interaction-mode))))

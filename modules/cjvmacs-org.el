61c1d34d82f8d768d8492014ecab8d2e65d230fd
Christian Vanderwall <christian@cvdub.net>
2026-01-28 17:44:19 -0800

    Right align org-agenda tags

--- modules/cjvmacs-org.el
+++ modules/cjvmacs-org.el
@@ -309,7 +309,7 @@
       "* TODO %i%?\n- %a")))
 
   ;; Agenda
-  (org-agenda-tags-column -120)
+  (org-agenda-tags-column 'auto)
   (org-agenda-window-setup 'current-window)
   (org-agenda-sorting-strategy '((agenda habit-down time-up scheduled-up priority-down)
                                  (todo priority-down category-keep)
